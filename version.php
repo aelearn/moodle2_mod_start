58ef06915fdd06f03b1adaadde5443ddf758374f
Daniel Del Rio <ddelrio1986@gmail.com>
2012-12-20 15:02:29 -0500

    Fix version file

--- version.php
+++ version.php
@@ -1,8 +1,8 @@
 <?php
 
-$plugin->version = 2012122000;
-$plugin->requires = 2012120300; // Moodle 2.4
-$plugin->cron = 0;
-$plugin->component = 'mod_start';
-$plugin->maturity = MATURITY_ALPHA;
-$plugin->release = '0.1 (Build: 2012122000)';
+$module->version = 2012122000;
+$module->requires = 2012120300; // Moodle 2.4
+$module->cron = 0;
+$module->component = 'mod_start';
+$module->maturity = MATURITY_ALPHA;
+$module->release = '0.1 (Build: 2012122000)';
