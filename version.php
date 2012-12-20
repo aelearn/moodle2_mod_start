650c3eb21b292649cb0bd5446480f6fd6219ad72
Daniel Del Rio <ddelrio1986@gmail.com>
2012-12-20 14:45:27 -0500

    Add the version file

---
+++ version.php
@@ -0,0 +1,8 @@
+<?php
+
+$plugin->version = 2012122000;
+$plugin->requires = 2012120300; // Moodle 2.4
+$plugin->cron = 0;
+$plugin->component = 'mod_start';
+$plugin->maturity = MATURITY_ALPHA;
+$plugin->release = '0.1 (Build: 2012122000)';
